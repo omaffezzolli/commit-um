3f5ddf2590c86d9569a06151a1d10c9b943148ea
joao <joao.l09@aluno.ifsc.edu.br>
2022-10-05 18:20:33 -0300

    ola

---
+++ commit_um/src/commit_um/primeiro.java
@@ -0,0 +1,19 @@
+package commit_um;
+
+public class primeiro {
+	
+	public static void main(String[] args) {
+		
+		int l[][] = new int[10] [10];
+		 for(int u = 0; u < 10; u++)
+		   {
+		      for(int d = 0; d < 10; d++)
+		      {
+		         System.out.printf("%2d ", l[u][d]);
+		      }
+		      System.out.println();
+		   }
+		}
+}
+
+
