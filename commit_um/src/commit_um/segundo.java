1fee1c0260049d7c772f506903f56f17b93b9d3b
joao <joao.l09@aluno.ifsc.edu.br>
2022-10-06 19:33:36 -0300

    ex 1,2

---
+++ commit_um/src/commit_um/segundo.java
@@ -0,0 +1,14 @@
+package commit_um;
+
+public class segundo {
+	
+	public static void main(String[] args) {
+		int nums[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+		int soma = 0;
+
+		for (int i : nums)
+		    soma += i;
+		System.out.println("O resultado da soma é " + soma);
+		}
+	}
+
